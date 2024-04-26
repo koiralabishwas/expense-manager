1309f87d68d94ca2b9f31ea0ba5eadccb4601f2d
BishwasKoirala <wasubisu69@gmail.com>
2024-04-26 17:43:40 +0900

    refactored the data

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import clientPromise from "@/lib/monogodb";
 import NavBar from "./components/NavBar";
 import { useFetch } from "./hooks/useFetchUser";
 import TransactionsTable from "./components/TransactionsTable";
+import TransactionForm from "./components/TransactionForm";
 
 export default function Home() {
   return (
    <div>
+    <TransactionForm></TransactionForm>
     <TransactionsTable></TransactionsTable>
    </div>
   );
