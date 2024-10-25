6fc0a81ceb07e9e30736d7640512f4933f1eaf58
Bishwas Koirala <wasubisu69@gmail.com>
2024-10-25 20:18:17 +0900

    init new frontend

--- frontend/src/app/providers/MaterialUiProviders.tsx
+++
@@ -1,5 +0,0 @@
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-
-export function MaterialUiProviders({ children }: { children: React.ReactNode }) {
-  return <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-}
