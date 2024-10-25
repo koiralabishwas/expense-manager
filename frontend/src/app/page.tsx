a6d6caac4fd3077fe30ab5105146be0884296d3b
Bishwas Koirala <wasubisu69@gmail.com>
2024-10-25 21:49:09 +0900

    made navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { Box, Heading } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function Home() {
+  let buttonLoading = false;
   return (
-    <div>Hello World</div>
+    <div>
+      <Header></Header>
+    </div>
   );
 }
