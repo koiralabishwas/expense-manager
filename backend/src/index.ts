4bedfc0eb451c4b9e3c29827f19575fa4cb04090
Bishwas Koirala <wasubisu69@gmail.com>
2024-11-15 02:34:32 +0900

    minor error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,15 +6,16 @@ import { incomeRoutes } from "./routes/incomeRoutes";
 import { expenseRoutes } from "./routes/expenseRoutes";
 import { authRoutes } from "./routes/authRoutes";
 import { authentication } from "./middlewares/authentication";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = new Hono();
 const protectedRoutes = new Hono();
-
+app.use("*" , errorHandler)
 app.route("/", home);
 app.route("/auth", authRoutes);
 
 // auth required routes
-// app.use("/api/*", authentication);
+app.use("/api/*", authentication);
 app.route("/api/users", userRoutes); // Set up user routes
 app.route("/api/incomes", incomeRoutes);
 app.route("/api/expenses", expenseRoutes);
