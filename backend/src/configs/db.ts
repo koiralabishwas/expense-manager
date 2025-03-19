993c4081bb8b29ca09dc63433f995b4768258a1d
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-19 23:09:05 +0900

    coment removal

--- backend/src/configs/db.ts
+++ backend/src/configs/db.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
 
 export const db = mongoose.connect(process.env.MONGODB_URI as string)
-  // .then(() => console.log("Connected to MongoDB"))t
-  // .catch((e) => console.error("MongoDB connection error:", e));
+  
