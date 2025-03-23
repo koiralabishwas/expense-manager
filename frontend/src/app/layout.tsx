b99b8cd36d70d5d58801787c058e2d5d57b8b8b2
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-24 01:45:01 +0900

    next auth failing

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopBar from "./components/TopBar";
+import Provider from "./Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,10 @@ export default function RootLayout({
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="container">
+          <Provider>
           <TopBar />
           <main className="main">{children}</main>
+          </Provider>
           <footer className="footer">© 2025 Your App</footer>
         </div>
       </body>
