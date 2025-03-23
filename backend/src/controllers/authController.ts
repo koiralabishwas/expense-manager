6ad4776c23a9b94adf7499ae3b86dc7c1b805b09
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-24 00:36:55 +0900

    minor auth fixes backend

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,49 +1,56 @@
 import { Context } from "hono";
 import Expense from "../models/expense";
 import User from "../models/user";
-import { sign , verify } from "hono/jwt";
-import {genSalt , hash ,compare} from 'bcryptjs';
-import {setCookie , getCookie} from "hono/cookie"
+import { sign, verify } from "hono/jwt";
+import { genSalt, hash, compare } from "bcryptjs";
+import { setCookie, getCookie } from "hono/cookie";
 
-export async function login(ctx:Context) {
-  const {email , password} = await ctx.req.json()
+export async function login(ctx: Context) {
+  const { email, password } = await ctx.req.json();
   // get user from emai and check password
   const user = await User.findOne({ email });
-  const isPassMatch = await compare(password ,user!.password)
-  if (!isPassMatch) return ctx.json({error : "Invalid "},401)
+  const isPassMatch = await compare(password, user!.password);
+  if (!isPassMatch) return ctx.json({ error: "Invalid " }, 401);
 
   const payload = {
-    _id : user?._id.toHexString(),
-    name : user?.name,
-    email : user?.email,
-    exp : Math.floor(Date.now() / 1000) * 7 * 24 * 60 * 60 // expires in 7 days
-  }
+    _id: user?._id.toHexString(),
+    name: user?.name,
+    email: user?.email,
+    exp: Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60, // expires in 7 days
+  };
 
-  const token = await sign(payload , process.env.JWT_SECRET!)
+  const token = await sign(payload, process.env.JWT_SECRET!);
 
   //TODO: look up this setCookie later
-  setCookie(ctx,"x-auth-token" , token , {
-    httpOnly : true,
-    secure : process.env.NODE_ENV === "production", //TODO: ここtrue?false?
-    sameSite : 'strict',
-    path : '/',
-    maxAge : 60 * 60 * 60
-  })
-  return ctx.json({message : "LogIn Successful" , payload , token})
+  setCookie(ctx, "x-auth-token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", //TODO: ここtrue?false?
+    sameSite: "strict",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60, // 7 days
+  });
+  return ctx.json({
+    message: "LogIn Successful",
+    user: { _id: payload._id, name: payload.name, email: payload.email },
+    token,
+  });
 }
 
-export async function register(ctx : Context) {
-  const {name , email , password} = await ctx.req.json()
-  // password hasing 
-  const existingUser = await User.findOne({email : email})
-  if (existingUser) return ctx.json({error :"user already exists with the given mail address"},400)
-  const hashedPassword = hash(password , await genSalt(10))
+export async function register(ctx: Context) {
+  const { name, email, password } = await ctx.req.json();
+  // password hasing
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser)
+    return ctx.json(
+      { error: "user already exists with the given mail address" },
+      400
+    );
+  const hashedPassword = hash(password, await genSalt(10));
   const user = new User({
     name: name,
     email: email,
-    password: await hashedPassword
-  })
-  await user.save()
-  return ctx.json({message : "register success" , user : {name , email}})
+    password: await hashedPassword,
+  });
+  await user.save();
+  return ctx.json({ message: "register success", user: { name, email } });
 }
-
