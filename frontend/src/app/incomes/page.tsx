d2468879833a008f588b0289aed93d20238e8121
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-31 16:49:39 +0900

    prototype fetching

--- frontend/src/app/incomes/page.tsx
+++ frontend/src/app/incomes/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import React from 'react'
 import PostIncome from './PostIncome'
+import { useSession } from 'next-auth/react'
 /**
  * TODO:
  * fetch Incomes ,
@@ -15,9 +17,24 @@ import PostIncome from './PostIncome'
  * refresh the get data
  */
 
+export const getIncomes = async (session : any) => {
+  const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + '/api/incomes' , {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${session.data.accessToken}`,
+    },
+  })
+  const income = await res.json()
+  return income
+} 
 const page = () => {
+  const session = useSession()
+  if (session.data?.accessToken) {
+    const data = getIncomes(session)
+    console.log(data)
+  }
   return (
-    <PostIncome></PostIncome>
+    <div>fetch incomes</div>
   )
 }
 
