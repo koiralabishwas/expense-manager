60ec02ce3915259a86f9a2d16372cb2f38530e52
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-31 23:35:46 +0900

    prototype expense form created using server actions

---
+++ frontend/src/app/expenses/action.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../lib/auth";
+
+export async function postExpense(prevState : any , formData : FormData) {
+
+  const session = await getServerSession(authOptions);
+
+  const formBody = {
+    description : formData.get('description'),
+    amount : formData.get('amount'),
+    currency : formData.get('currency'),
+    genre : formData.get('genre')
+  }
+
+  const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + "/api/expenses",{
+    method : "POST",
+    body : JSON.stringify(formBody),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.accessToken}`,
+    }
+  })
+
+  const json = await res.json()
+
+  if(!res.ok){
+    return {message : "request failed"}
+  }
+  return {mesage : "submit success"}
+}
--- frontend/src/app/expenses/page.tsx
+++ frontend/src/app/expenses/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+import React, { useActionState } from 'react'
+import { postExpense } from './action'
+
+const initialState = {
+  message : ""
+}
 
 const page = () => {
+  const [state ,formAction , pending] = useActionState(postExpense , initialState)
   return (
-    <div>Expense Page</div>
+    <form action={formAction}>
+    <label htmlFor="description">description</label>
+    <input type="text" id="description" name="description" required />
+    <label htmlFor="Amount">Amount</label>
+    <textarea id="amount" name="amount" required />
+    <label htmlFor="Currency">Currency</label>
+    <textarea id="currecy" name="currency" required />
+    <label htmlFor="Genre">Genre</label>
+    <textarea id="genre" name="genre" required />
+    {state?.message && <p aria-live="polite">{state.message}</p>}
+    <button disabled={pending}>Create Post</button>
+  </form>
   )
 }
 
