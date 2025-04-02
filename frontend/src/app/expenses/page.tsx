cda01b8bf9d1f4a456f02ea0dc6289bbd8d09ed4
Bishwas Koirala <wasubisu69@gmail.com>
2025-04-02 23:15:07 +0900

    perfect form action component created mah boi

--- frontend/src/app/expenses/page.tsx
+++ frontend/src/app/expenses/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useActionState } from 'react'
 import { postExpense } from './action'
+import { Box, Button, TextField } from '@mui/material'
 
 const initialState = {
   message : ""
@@ -10,16 +11,14 @@ const page = () => {
   const [state ,formAction , pending] = useActionState(postExpense , initialState)
   return (
     <form action={formAction}>
-    <label htmlFor="description">description</label>
-    <input type="text" id="description" name="description" required />
-    <label htmlFor="Amount">Amount</label>
-    <textarea id="amount" name="amount" required />
-    <label htmlFor="Currency">Currency</label>
-    <textarea id="currecy" name="currency" required />
-    <label htmlFor="Genre">Genre</label>
-    <textarea id="genre" name="genre" required />
-    {state?.message && <p aria-live="polite">{state.message}</p>}
-    <button disabled={pending}>Create Post</button>
+      <Box maxWidth={400} mx="auto" mt={6} px={2}>
+        <TextField label="description" id='description' name='description' required/>
+        <TextField label="amount" id='amount' name='amount' required/>
+        <TextField label="currency" id='currency' name='currency' required/>
+        <TextField label="genre" id='genre' name='genre' required/>
+        {state?.message && <p aria-live="polite">{state.message}</p>}
+        <Button type='submit' variant='contained' fullWidth sx={{mt:2}} >{pending ? "submitting.." : "submit"}</Button>
+      </Box>
   </form>
   )
 }
