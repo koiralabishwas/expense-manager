8816cb37672e6f462018c5c35b00b72273ef87e3
Bishwas Koirala <wasubisu69@gmail.com>
2025-06-23 15:43:38 +0900

    refactored income controller with proper rest 思想

--- backend/src/controllers/incomeController.ts
+++ backend/src/controllers/incomeController.ts
@@ -1,52 +1,65 @@
 import { Context } from "hono";
-import Income from '../models/income'
+import Income from "../models/income";
 
-export async function getUserIncomes(ctx : Context) {
+export async function getUserIncomes(ctx: Context) {
   try {
-    const {_id} = ctx.get('user')
-    const userIncome = await Income.find({userId : _id})
-    return ctx.json(userIncome)
-} catch (error) {
-    return ctx.json({error} , 401)
+    const { _id } = ctx.get("user");
+    const yearMonth = ctx.req.query("yearMonth") ?? null;
+
+    const userIncome = await Income.find({
+      userId: _id,
+      ...(yearMonth && { yearMonth }), 
+    });
+    return ctx.json(userIncome);
+  } catch (error) {
+    return ctx.json({ error }, 400);
   }
 }
 
-export async function postUserIncome(ctx : Context) {
+export async function postUserIncome(ctx: Context) {
   try {
-    const user = ctx.get('user')
-    const body = await ctx.req.json()
+    const user = ctx.get("user");
+    const body = await ctx.req.json();
     const newUserIncome = await new Income({
-      userId : user._id,
-      description : body.description,
-      amount : body.amount , 
-      currecny : body.currency,
-      genre : body.genre
-    }).save()
-    return ctx.json(newUserIncome)
+      userId: user._id,
+      description: body.description,
+      yearMonth: body.yearMonth,
+      amount: body.amount,
+      currency: body.currency,
+      genre: body.genre,
+    }).save();
+    return ctx.json(newUserIncome);
   } catch (error) {
-    return ctx.json({error , info : "unexpected Error"} , 401)
+    return ctx.json({ error, info: "unexpected Error" }, 401);
   }
 }
 
-export async function editUserIncome(ctx:Context) {
-  try{
-    const user = ctx.get('user')
-    const incomeId = ctx.req.param('incomeId')
-    const body = await ctx.req.json()
-    const changedUserIncome = await Income.findOneAndUpdate({_id : incomeId , userId : user._id},body,{new : true , runValidators : true})
-    return ctx.json(changedUserIncome)
-  } catch (error) {
-    return ctx.json({error},400)
-  }
-}
-
-export async function deleteUserIncome(ctx:Context) {
+export async function editUserIncome(ctx: Context) {
   try {
-    const user = ctx.get('user')
-    const incomeId = ctx.req.param('incomeId')
-    const deletedUserIncome = await Income.findOneAndDelete({_id : incomeId , userId : user._id})
-    return ctx.json(deletedUserIncome)
+    const user = ctx.get("user");
+    const incomeId = ctx.req.param("incomeId");
+    const body = await ctx.req.json();
+    const changedUserIncome = await Income.findOneAndUpdate(
+      { _id: incomeId, userId: user._id },
+      body,
+      { new: true, runValidators: true }
+    );
+    return ctx.json(changedUserIncome);
   } catch (error) {
-    return ctx.json({error} , 400)
+    return ctx.json({ error }, 400);
+  }
+}
+
+export async function deleteUserIncome(ctx: Context) {
+  try {
+    const user = ctx.get("user");
+    const incomeId = ctx.req.param("incomeId");
+    const deletedUserIncome = await Income.findOneAndDelete({
+      _id: incomeId,
+      userId: user._id,
+    });
+    return ctx.json(deletedUserIncome);
+  } catch (error) {
+    return ctx.json({ error }, 400);
   }
 }
