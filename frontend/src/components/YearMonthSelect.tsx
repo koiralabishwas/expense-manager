b54e1ea66353d6955e5aea1cb13b0d474dd17397
Bishwas Koirala <wasubisu69@gmail.com>
2025-07-31 15:23:02 +0900

    fix : fix select yearMonth error

--- frontend/src/components/YearMonthSelect.tsx
+++ frontend/src/components/YearMonthSelect.tsx
@@ -1,22 +1,32 @@
 "use client";
-import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { useSearchParams } from "next/navigation";
+import { useId, useMemo } from "react";
 
 export default function YearMonthSelect() {
   const now = new Date();
-
   const currentYear = now.getFullYear();
   const currentMonth = (now.getMonth() + 1).toString().padStart(2, "0");
-  const yearMonthOptions = Array.from({ length: 12 }, (_, i) => {
-    const date = new Date(now);
-    date.setMonth(now.getMonth() - 6 + i); // start from -4 months
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    return `${year}${month}`;
-  });
+  const baseDate = new Date(currentYear, now.getMonth(), 1); // 1st day of current month
+
+  const yearMonthOptions = useMemo(() => {
+    return Array.from({ length: 12 }, (_, i) => {
+      const date = new Date(baseDate);
+      date.setMonth(baseDate.getMonth() - 6 + i);
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      return `${year}${month}`;
+    });
+  }, [baseDate]);
 
   const searchParams = useSearchParams();
-
   const selectedYearMonth =
     searchParams.get("yearMonth") || `${currentYear}${currentMonth}`;
 
@@ -27,13 +37,14 @@ export default function YearMonthSelect() {
     window.location.search = params.toString();
   };
 
+  const id = useId();
+
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
-        <InputLabel id="yearMonth-label">年月</InputLabel>
+        <InputLabel >年月</InputLabel>
         <Select
-          labelId="yearMonth-label"
-          id="yearMonth"
+          id={id}
           value={selectedYearMonth}
           label="年月"
           onChange={handleChange}
