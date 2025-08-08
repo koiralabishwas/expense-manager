62e0f7480d68974f88e2e7fdf0b940265e2e581c
Bishwas Koirala <wasubisu69@gmail.com>
2025-08-08 23:39:54 +0900

    hotfix : protected all routes so it will automatically logout in home page to prevent client side rendering error

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const PROTECTED_PATHS = ["/dashboard", "/expenses", "/incomes"];
+const PROTECTED_PATHS = ["/dashboard", "/expenses", "/incomes" , "/"];
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
