23b18672ed6b89f6292aae5d6156286985eebd1f
Bishwas Koirala <wasubisu69@gmail.com>
2025-12-11 18:30:43 +0900

    remove backend

--- backend/src/routes/incomeRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-import { postUserIncome, deleteUserIncome, editUserIncome, getUserIncomes } from '../controllers/incomeController';
-
-export const incomeRoutes = new Hono();
-
-incomeRoutes.get('/',getUserIncomes)
-incomeRoutes.post('/',postUserIncome)
-incomeRoutes.put('/:incomeId',editUserIncome)
-incomeRoutes.delete('/:incomeId',deleteUserIncome)
--- backend/src/routes/userRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono';
-import {getLoggedUser, updateLoggedUser, deleteLoggedUser } from '../controllers/userController';
-
-export const userRoutes = new Hono();
-userRoutes.get('/',getLoggedUser);
-userRoutes.put('/' , updateLoggedUser)
-userRoutes.delete('/' , deleteLoggedUser)
