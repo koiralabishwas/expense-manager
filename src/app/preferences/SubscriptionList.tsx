fad6595e1a5d48f9dd7b2e1c04519fabb27b85e2
Bishwas Koirala <wasubisu69@gmail.com>
2026-01-03 16:55:20 +0900

    add delete subscription button

--- src/app/preferences/SubscriptionList.tsx
+++ src/app/preferences/SubscriptionList.tsx
@@ -1,25 +1,46 @@
 "use client"
+import { deleteSubscription } from '@/server/preference.server'
 import { Subscription } from '@/types/user'
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
+import { Button, Typography } from '@mui/material'
+import { Box, flex } from '@mui/system'
+import { useQueryClient } from '@tanstack/react-query'
+import React, { useState } from 'react'
+import SubscriptionForm from './SubscriptionForm'
 
 interface Props {
-  subscriptions : Subscription[]
+  subscriptions: Subscription[]
 }
 
-const SubscriptionList = ({subscriptions} : Props) => {
+const SubscriptionList = ({ subscriptions }: Props) => {
+  const queryClient = useQueryClient()
   return (
     <Box>
       {subscriptions.map((s, n) =>
-        <Box px={2} pb={2} key={n}>
-          {/* <Typography>{s.name}</Typography> */}
-          <Typography>{s.name}</Typography>
-          <Typography>{s.amount}円</Typography>
-          <Typography>{s.paymentDay}日支払い</Typography>
-          <Typography>{s.isActive ? "契約中" : "停止中"}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+
+          <Box px={2} pb={2} key={n}>
+            {/* <Typography>{s.name}</Typography> */}
+            <Typography>{s.name}</Typography>
+            <Typography>{s.amount}円</Typography>
+            <Typography>{s.paymentDay}日支払い</Typography>
+            <Typography>{s.isActive ? "契約中" : "停止中"}</Typography>
+          </Box>
+          <Box sx={{
+            display: "flex",
+            flexDirection: "column"
+          }}>
+            <Button sx={{ color: "green", border: "0.1rem solid green", mb: "0.1rem" }}>編集</Button>
+            <Button onClick={() => { deleteSubscription(s._id!); queryClient.invalidateQueries({ queryKey: ["user"] }) }} sx={{ color: "red", border: "0.1rem solid red", mb: "0.1rem" }}>削除</Button>
+
+          </Box>
         </Box>
       )}
+      
     </Box>
   )
 }
